a65af0beca7ba3245d786a2b118690ce85d1d1e0
MaruKazeMaru <kaze47111@gmail.com>
2023-03-05 17:03:39 +0900

    fix settings

--- kazesite/settings.py
+++ kazesite/settings.py
@@ -34,14 +34,14 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
-	'''
+    '''
     import environ
     env = environ.Env()
     env.read_env(os.path.join(BASE_DIR,'.env'))
 
     SECRET_KEY = env('SECRET_KEY')
     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-'''
+    '''
     from .global_settings import SECRET_KEY, ALLOWED_HOSTS, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'

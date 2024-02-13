829756417ed3854c366e1f0f2261f906d5558cbc
MaruKazeMaru <kaze47111@gmail.com>
2024-02-13 23:09:07 +0900

    add app:main & rename site

--- kazesite/asgi.py
+++ kazesite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for wwwmkm project.
+ASGI config for kazesite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wwwmkm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kazesite.settings')
 
 application = get_asgi_application()
--- kazesite/wsgi.py
+++ kazesite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wwwmkm project.
+WSGI config for kazesite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wwwmkm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kazesite.settings')
 
 application = get_wsgi_application()

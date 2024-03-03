cc587e6ea802bd9d5b98b70e4f0f612bdcd54883
MaruKazeMaru <kaze47111@gmail.com>
2024-03-03 23:32:18 +0900

    add wili

--- wili/urls.py
+++ wili/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "wili"
+
 urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("tools", views.ToolsIndexView.as_view(), name="tools_index"),
+    path("tools/reference", views.ToolsRefView.as_view(), name="tools_ref"),
 ]
--- wili/views.py
+++ wili/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = "wili/index.html"
+
+
+class ToolsIndexView(TemplateView):
+    template_name = "wili/tools/index.html"
+
+
+class ToolsRefView(TemplateView):
+    template_name = "wili/tools/ref.html"
